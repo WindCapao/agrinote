4c6ee10e08204e8156945ab9d661dc7e5c173f68
Stephanie Natividad <natividadstephaniem@gmail.com>
2025-12-27 17:13:46 +0800

    update

--- database/migrations/2025_12_27_090430_add_role_to_users_table.php
+++ database/migrations/2025_12_27_090430_add_role_to_users_table.php
@@ -10,19 +10,17 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->enum('role', ['user', 'admin'])->default('user')->after('email');
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+public function down(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn('role');
+    });
+}
+
 };
