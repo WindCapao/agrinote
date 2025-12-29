228f6e64cae7fb22337740dbaf85da0bf9331eb8
Stephanie Natividad <natividadstephaniem@gmail.com>
2025-12-29 23:32:35 +0800

    create new migrations

--- database/migrations/2025_12_27_090429_create_categories_table.php
+++ database/migrations/2025_12_27_090429_create_categories_table.php
@@ -6,25 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-   public function up(): void
-{
-    Schema::create('categories', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->string('slug')->unique();
-        $table->text('description')->nullable();
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
 
-    /**
-     * Reverse the migrations.
-     */
+        Schema::create('article_category', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('article_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
     public function down(): void
     {
+        Schema::dropIfExists('article_category');
         Schema::dropIfExists('categories');
     }
 };
