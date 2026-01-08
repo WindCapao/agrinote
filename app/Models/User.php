f6d646ff16456599000bc185442c13e2236bb6de
Stephanie Natividad <natividadstephaniem@gmail.com>
2026-01-09 02:00:50 +0800

    update langs

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,10 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+   
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
      * @var list<string>
      */
     protected $fillable = [
@@ -25,8 +23,6 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
      * @var list<string>
      */
     protected $hidden = [
@@ -35,8 +31,6 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
      * @return array<string, string>
      */
     protected function casts(): array
@@ -47,17 +41,11 @@ class User extends Authenticatable
         ];
     }
 
-    /**
-     * A user can have many articles
-     */
     public function articles()
     {
         return $this->hasMany(Article::class);
     }
 
-    /**
-     * Check if user is admin
-     */
     public function isAdmin()
     {
         return $this->role === 'admin';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,17 +4,17 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__)) //set base path
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-    ]);
+    ->withMiddleware(function (Middleware $middleware) {  //register middleware
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,  //admin check
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {  //register exception handlers
         //
     })->create();
